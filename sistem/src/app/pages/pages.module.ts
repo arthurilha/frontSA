64ce40119a78503d42a95a37019bd9338aa2862c
Arthur Battistel Ilha <arthur.ilha20@gmail.com>
2023-10-17 00:43:56 +0000

    first

---
+++ sistem/src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PagesRoutingModule } from './pages-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class PagesModule { }
